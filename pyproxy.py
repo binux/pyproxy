b0f9fbb410536ad7e0bd38ad8a1a488e1420f231
binux <roy@binux.me>
2015-01-28 21:37:27 +0800

    add setup.py

--- pyproxy.py
+++ pyproxy.py
@@ -5,6 +5,8 @@
 #         http://binux.me
 # Created on 2012-12-15 16:11:13
 
+__version__ = '0.1'
+
 import logging
 import tornado.web
 from tornado.options import define, options
---
+++ setup.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+# vim: set et sw=4 ts=4 sts=4 ff=unix fenc=utf8:
+# Author: Binux<roy@binux.me>
+#         http://binux.me
+# Created on 2015-01-28 21:31:13
+
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+
+import pyproxy
+setup(
+    name='pyproxy',
+    version=pyproxy.__version__,
+
+    description='An HTTP proxy server with API on tornado, just in one file!',
+    long_description=long_description,
+
+    url='https://github.com/binux/pyproxy',
+
+    author='Roy Binux',
+    author_email='roy@binux.me',
+
+    license='Apache License, Version 2.0',
+
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+
+        'License :: OSI Approved :: Apache Software License',
+
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+
+        'Topic :: Internet :: WWW/HTTP',
+    ],
+
+    keywords='proxy https http',
+
+    packages=find_packages(exclude=[]),
+
+    install_requires=[
+        'tornado>=2.1.1',
+    ],
+
+    entry_points={
+        'console_scripts': [
+            'pyproxy=pyproxy:main'
+        ]
+    },
+)
